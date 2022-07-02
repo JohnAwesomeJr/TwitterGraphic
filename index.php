8f58e2034b15034d520078bef2b7403a32923415
John Christensen <johnawesomejr@gmail.com>
2022-07-02 16:10:30 -0500

    finished the count function

--- index.php
+++ index.php
@@ -21,7 +21,10 @@
     border-radius: 10px;
     font-size: 20px;
 }
-</style>
+
+p {
+    margin-bottom: 20px;
+}
 </style>
 
 <body>
@@ -29,51 +32,52 @@
     <?php include_once('uiElements/varType.php'); ?>
     <?php include_once('uiElements/functionMaker.php'); ?>
     <?php include_once('uiElements/listOfDatatypes.php'); ?>
-
-
-
-
     <?php
     $function = new functionMaker();
     $varType = new varType();
     $backdrop = new backdrop();
     $dataTypesBox = new listOfDatatypes();
+    $arrayType1 = $varType->typeArray('$Array', 1);
+    $intType1 = $varType->typeInteger('$integer', 1);
+
+    $intType1second = $varType->typeInteger('0', 1);
+    $intType1third = $varType->typeInteger('1', 1);
 
 
-    $typeString1 = $varType->typeString('$String', 1);
-    $typeInteger1 = $varType->typeInteger('$FirstInteger', 1);
-    $typeInteger2 = $varType->typeInteger('$SecondInteger', 1);
-
-
-
-
-
-
-
-    $functionName = "substr";
-    $variableArray = [$typeString1, $typeInteger1, $typeInteger2];
+    $functionName = "count";
+    $variableArray = [$arrayType1, $intType1];
     $completeFunction = $function->makeFunction($functionName, $variableArray);
-
-    $explanation = "<h1>{$functionName}</h1><br><br>substr — Return part of a string.<br><br>";
+    $firstExsample = $function->makeFunction($functionName, [$arrayType1, $intType1second]);
+    $firstExsample1 = $function->makeFunction($functionName, [$arrayType1]);
+    $secondExsample = $function->makeFunction($functionName, [$arrayType1, $intType1third]);
+    $explanation = <<<EOD
+    <h1>{$functionName}</h1><br>
+    count — Counts all elements in an array or in a Countable object.
+    <br><br>
+    EOD;
     $useCase = <<<EOD
     <br>
-    {$varType->typeInteger('$FirstInteger', 0)} Is the starting position of the selection. {$varType->typeInteger('$SecondInteger', 0)} is the number of characters that you want to select. if {$varType->typeInteger('$SecondInteger', 0)} is not given it will select everything after {$varType->typeInteger('$FirstInteger', 0)}
-    
-    
-    <br>
-    If {$varType->typeInteger('$FirstInteger', 0)} is a negative number than the selection will be reversed. It will start at the end of the string.
-    eg:<br><br>
     <pre><code class="language-php">
-    \$rest = substr("abcdef", -1);    // returns "f"
-    \$rest = substr("abcdef", -2);    // returns "ef"
-    \$rest = substr("abcdef", -3, 1); // returns "d"
-
-    \$rest = substr("abcdef", 0, -1);  // returns "abcde"
-    \$rest = substr("abcdef", 2, -1);  // returns "cde"
-    \$rest = substr("abcdef", 4, -4);  // returns "";
-    \$rest = substr("abcdef", -3, -1); // returns "de"
+    \$array = Array
+    (
+        [0] => 'el1'
+        [1] => 'el2'
+        [2] => 'el3'
+        [3] => Array
+            (
+                [0] => 'newArrayEl1'
+                [1] => 'newArrayEl2'
+                [2] => 'newArrayEl3'
+                [3] => 'newArrayEl4'
+            )
+    
+    )
     </code></pre>
     <br><br>
+    <p>If the array looks like the example above, entering a 0 in the second argument will <b>return 4</b> because there are only four elements in the first level of the array.</p>
+    {$firstExsample} &nbsp;&nbsp;&nbsp; <b>or</b> &nbsp;&nbsp;&nbsp; {$firstExsample1}
+    <p>if you enter a 1 in the second argument it will <b>return 8</b> because it counts all of the elements, even if the aray is multidimensional with arrays inside arrays.</p>
+    {$secondExsample}
     EOD;
 
     $fullFunction = $backdrop->indentWindow($completeFunction);
@@ -81,14 +85,11 @@
 
     $combine = $explanation . $datatypesBoxShow . $fullFunction . $useCase;
     $window1 = $backdrop->windowGen($combine);
-
     $windows = $window1;
     echo $backdrop->makeBackdrop(
         $windows
     );
-
     ?>
-
     <script>
     hljs.highlightAll();
     </script>
